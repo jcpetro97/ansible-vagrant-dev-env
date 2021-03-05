094529bad0c7a8e58e8832ce0616910daa8e3e84
John Petro <john@johnpetro.com>
2021-03-05 15:51:44 -0500

    Finished getting stuff configured so that a vagrant test env could be set up.

---
+++ ubuntu2004/provisioning/ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Python.
+/bin/echo "------> installing python3 <------"
+/usr/bin/apt update
+/usr/bin/apt -y install python3 python3-pip
+/bin/echo "------> upgrade pip <------"
+/usr/bin/python3 -m pip install --upgrade pip
+/bin/echo "------> upgrade setuptools <------"
+/usr/bin/python3 -m pip install --upgrade setuptools
+# Install Ansible.
+/bin/echo "------> Install Ansible 2.9 <------"
+/usr/bin/python3 -m pip install ansible==2.9.17
+#
+/bin/echo "------> Verify Ansible Version <------"
+/usr/local/bin/ansible --version
