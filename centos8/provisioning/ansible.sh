0064d4f319d0de11ed8542cecc2a23edd08e625b
John Petro <john@johnpetro.com>
2021-03-04 22:22:55 -0500

    updated centos8 Vagrant file to use ansible.sh to install ansible 2.9.17

---
+++ centos8/provisioning/ansible.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Python.
+/usr/bin/echo "------> installing python3 <------"
+/usr/bin/yum -y install python3 python3-pip
+/usr/bin/echo "------> upgrade pip <------"
+/usr/bin/python3 -m pip install --upgrade pip
+/usr/bin/echo "------> upgrade setuptools <------"
+/usr/bin/python3 -m pip install --upgrade setuptools
+# Install Ansible.
+/usr/bin/echo "------> Install Ansible 2.9 <------"
+/usr/bin/python3 -m pip install ansible==2.9.17
+#
+/usr/bin/echo "------> Verify Ansible Version <------"
+/usr/local/bin/ansible --version
